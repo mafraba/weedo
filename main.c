03eb2b6d0580b1e05326b32ab9dc0145b633ff86
Manuel Franco <mafraba@gmail.com>
2012-08-24 23:50:24 +0200

    Orientaciones diferentes. Nombre de ficheros.

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
-/* 
+/*
+ * Application of gabor filters to classify sky, terrain, corn and weeds
+ * in agricultural images.
+ * 
+ * References:
+ * 
+ * [1] D. Clausi, M. Ed Jernigan, ”Designing Gabor filters for optimal texture 
+ * separability,” Pattern Recognition, vol. 33, pp. 1835-1849, 2000.
  * 
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <opencv/cv.h>
@@ -13,6 +19,12 @@
 #define ORIGINAL_IMAGE_WINDOW_NAME "Original image"
 #define OUTPUT_PATH "/tmp/weedo"
 
+unsigned int bandwidths[1] = {8};
+// Orientations as recommended in [1]
+float orientations[6] = {0, PI / 6, 2 * PI / 6, 3 * PI / 6, 4 * PI / 6, 5 * PI / 6};
+// Spatial frequencies
+float spatial_frequencies[4] = {1, 2, 3, 4};
+
 void show(char* name, CvArr* img)
 {
     cvNamedWindow(name, CV_WINDOW_AUTOSIZE);
@@ -21,24 +33,55 @@ void show(char* name, CvArr* img)
 
 void output_filtered_images(char *prefix, unsigned int n, CvArr** imgs)
 {
-    for (int i = 0; i < n; i++)
+    int i = 0;
+    for (int frq = 0; frq < 4; frq++)
     {
-        char out_file_name[256];
-        sprintf(out_file_name, "%s/%s_%s_%02d.png", OUTPUT_PATH, prefix, "FLT", i);
-        puts(out_file_name);
-        cvSaveImage(out_file_name, imgs[i], NULL);
+        for (int orn = 0; orn < 6; orn++)
+        {
+            char out_file_name[256];
+            sprintf(out_file_name, "%s/%s_%s_%02d_%02.2f_%02.2f.png",
+                    OUTPUT_PATH,
+                    prefix,
+                    "FLT",
+                    bandwidths[0],
+                    spatial_frequencies[frq],
+                    orientations[orn]);
+            puts(out_file_name);
+            cvSaveImage(out_file_name, imgs[i], NULL);
+            i++;
+        }
     }
+}
+
+void chromacity(CvMat* img)
+{
+    int pixels = img->rows * img->cols;
+    int channels = 3; // assumed
+
+    for (int i = 0; i < pixels * channels; i += channels)
+    {
+        unsigned char red = img->data.ptr[i + 2];
+        unsigned char green = img->data.ptr[i + 1];
+        unsigned char blue = img->data.ptr[i];
+
+        double total = red + green + blue;
+        unsigned char r = round(red * 255.0 / total);
+        unsigned char g = round(green * 255.0 / total);
+        unsigned char b = round(blue * 255.0 / total);
+        
+        img->data.ptr[i+2] = r;
+        img->data.ptr[i+1] = g;
+        img->data.ptr[i] = b;
+    }
+
 }
 
 int main(int argc, char** argv)
 {
-    unsigned int bandwidths[1] = {8};
-    float orientations[4] = {0, PI / 2, PI / 4, -PI / 4};
-    float spatial_frequencies[4] = {1, 2, 3, 4};
-
     // Load and display original image
     puts("Loading image...");
     CvMat* img = cvLoadImageM(PATH, CV_LOAD_IMAGE_COLOR);
+    chromacity(img);
     show(ORIGINAL_IMAGE_WINDOW_NAME, img);
 
     // Generate a Gabor filter bank
@@ -47,7 +90,7 @@ int main(int argc, char** argv)
     generate_gabor_filter_bank(&filter_bank,
                                1, bandwidths,
                                4, spatial_frequencies,
-                               4, orientations);
+                               6, orientations);
 
     // Separate each channel
     puts("Separating channels...");
